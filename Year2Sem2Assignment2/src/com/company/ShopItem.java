f98cebce29fdd1a48c782e151250c36ddc9666b8
John Michael De Guzman <jmxdg1029@gmail.com>
2021-03-27 16:47:23 -0400

    DataStruct StarterCode

---
+++ Year2Sem2Assignment2/src/com/company/ShopItem.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ShopItem {
+    Weapon item;
+    int numberInStock;
+
+    public ShopItem(Weapon w, int nInStock){
+        item=w;
+        numberInStock=nInStock;
+    }
+}
+
+
