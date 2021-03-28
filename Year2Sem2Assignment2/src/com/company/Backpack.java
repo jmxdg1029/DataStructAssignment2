f204e9b160f96791c22185c2f1120160e62c8d44
John Michael De Guzman <jmxdg1029@gmail.com>
2021-03-27 20:51:32 -0400

    backpack part 1

--- Year2Sem2Assignment2/src/com/company/Backpack.java
+++ Year2Sem2Assignment2/src/com/company/Backpack.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Backpack {
+    private int numItems;
+    private int maxItems ;
+    private double currWeight;
+    private double maxWeight ;
+
+    public Backpack(){
+        numItems = 0;
+        maxItems = 30;
+        currWeight = 0;
+        maxWeight = 90;
+    }
 }
