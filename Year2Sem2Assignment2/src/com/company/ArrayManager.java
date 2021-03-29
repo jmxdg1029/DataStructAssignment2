105b4de86b16f8f3348d8561549640a2b08d2262
John Michael De Guzman <jmxdg1029@gmail.com>
2021-03-28 21:29:19 -0400

    kinda okay broke nkina array out of boubnds

--- Year2Sem2Assignment2/src/com/company/ArrayManager.java
+++ Year2Sem2Assignment2/src/com/company/ArrayManager.java
@@ -1,23 +1,42 @@
 package com.company;
 
 public class ArrayManager {
-
+    double loadFactor;
     int maxItems;    // records the max size of the table
     int numItems;       // records number of items in the list
     ShopItem[] table; //hashtable itself
 
     public ArrayManager(int size)
     {
-        maxItems = size;
-        numItems = 0;
         table = new ShopItem[maxItems];
+        maxItems = size;
+        numItems = 1;
+        loadFactor = 0.75;
     }
 
-    public void put(Weapon item,int quantity)
-    {
-        if (numItems<maxItems){
-            table[numItems] = new ShopItem(item,quantity);
+    public int hashFunction(Weapon item){
+        int value = 0,weight = 1;
+        for(int x = 1; x < item.weaponName.length(); x++){
+            value+=(item.weaponName.charAt(x)-'a'+1)*weight;
+            weight++;
+        }
+        value+= item.range+item.damage;
+        return value%maxItems;
+    }
+
+    //insert
+    public void put(Weapon item,int quantity) {
+        int count = 1;//number of collision
+        if (numItems/maxItems <loadFactor){
+            int startLoc = hashFunction(item);
+            int loc = startLoc;
+            while(table[loc] != null) {
+                loc = (startLoc+count*count)%maxItems;
+                count++;
+            }
+            table[loc] = new ShopItem(item,quantity);
             numItems++;
+            System.out.println(numItems);
         }
 
     }
